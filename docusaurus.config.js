b95119da12f708f1daf1939347ff9bcb35175fff
Grégory Heitz <324670+edno@users.noreply.github.com>
2023-11-09 11:38:20 +0000

    fix docusaurus config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,8 +1,9 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require("prism-react-renderer/themes/github");
-const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+const { themes } = require("prism-react-renderer");
+const lightCodeTheme = themes.github,
+  darkCodeTheme = themes.dracula;
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -24,10 +25,10 @@ const config = {
         blog: false,
         docs: {
           routeBasePath: "/",
-          sidebarPath: require.resolve("./sidebars.js")
+          sidebarPath: "./sidebars.js",
         },
         theme: {
-          customCss: require.resolve("./src/css/custom.css")
+          customCss: "./src/css/custom.css",
         }
       })
     ]
